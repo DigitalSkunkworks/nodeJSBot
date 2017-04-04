0a12a679eb876b37ab6820006ecb36bf021dcfd4
liambarnett <liamwba@hotmail.com>
2017-04-04 16:44:48 +0100

    removed keys

--- server.js
+++ server.js
@@ -10,11 +10,18 @@ const SkypeBotConfig = require('./skypebotconfig');
 
 const REST_PORT = (process.env.PORT || 5000);
 
-const botConfig = new SkypeBotConfig(
+/*const botConfig = new SkypeBotConfig(
     "725e3e5c57f94dfebdfa12d81527fa38",
     "en",
     "355a4537-d750-4064-a4b4-45416960b2e5",
     "iWvLUFBSdHbRUE9xAfn9tf0" 
+);*/
+
+const botConfig = new SkypeBotConfig(
+    process.env.APIAI_ACCESS_TOKEN,
+    process.env.APIAI_LANG,
+    process.env.APP_ID,
+    process.env.APP_SECRET
 );
 
 const skypeBot = new SkypeBot(botConfig);
